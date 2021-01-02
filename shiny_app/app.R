a186118c822acf7ebff0c2525fb4ea12d625b8fc
Econundrums <econundrums7@gmail.com>
2021-01-01 16:22:27 -0800

    Updated app to calculate odds per state and allow user to tune the percentage of voters.

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -7,66 +7,54 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
-library(shinythemes)
-library(dplyr)
+# To Do:
+# 1. Choropleth map of varying state odds.
+# 2. Lottery comparisons.
+# 3. Make it look "pretty".
+# 4. Binomial distribution plot?
 
+library(shiny)
+library(leaflet)
+library(maps)
+library(dplyr)
+library(readxl)
+
+
+statePop = read_excel("../data/master.xlsx")
+stateVect = statePop$Total
+names(stateVect) = statePop$State
+    
+votingOdds = function(population){
+    probability = dbinom(round(population*0.5), size = population, prob = 0.5)
+    result = paste(round(probability*100, 4), "%", " or 1 in ", 
+                   round(1/probability), sep = "")
+    return(result)
+}
 
 ui <- fluidPage(
     titlePanel("Odds Your Vote Matters"),
     
     selectInput(inputId = "state", 
-                label = "State: ",
-                choices = c("Alabama" = "AL",
-                            "Alaska" = "AK",
-                            "Arizona" = "AZ",
-                            "Arkansas" = "AR",
-                            "California" = "CA",
-                            "Colorado" = "CO",
-                            "Connecticut" = "CT",
-                            "Delaware" = "DE",
-                            "Florida" = "FL",
-                            "Georgia" = "GA",
-                            "Hawaii" = "HI",
-                            "Idaho" = "ID",
-                            "Illinois" = "IL",
-                            "Indiana" = "IN",
-                            "Iowa" = "IA",
-                            "Kansas" = "KS",
-                            "Kentucky" = "KY",
-                            "Louisiana" = "LA",
-                            "Maine" = "ME",
-                            "Maryland" = "MD",
-                            "Massachusetts" = "MA",
-                            "Michigan" = "MI",
-                            "Minnesota" = "MN",
-                            "Mississippi" = "MS",
-                            "Missouri" = "MO",
-                            "Montana" = "MT",
-                            "Nebraska" = "NE",
-                            "Nevada" = "NV",
-                            "New Hampshire" = "NH",
-                            "New Jersey" = "NJ",
-                            "New Mexico" = "NM",
-                            "New York" = "NY",
-                            "North Carolina" = "NC",
-                            "North Dakota" = "ND",
-                            "Ohio" = "OH",
-                            "Oklahoma" = "OK",
-                            "Oregon" = "OR",
-                            "Pennsylvania" = "PA",
-                            "Rhode Island" = "RI",
-                            "South Carolina" = "SC",
-                            "South Dakota" = "SD",
-                            "Tennessee" = "TN",
-                            "Texas" = "TX",
-                            "Utah" = "UT",
-                            "Vermont" = "VT",
-                            "Virginia" = "VA",
-                            "Washington" = "WA",
-                            "West Virginia" = "WV",
-                            "Wisconsin" = "WI",
-                            "Wyoming" = "WY")),
+                label = "Pick Your State: ",
+                choices = c("Alabama", "Alaska", "Arizona", "Arkansas", 
+                            "California", "Colorado", "Connecticut", "Delaware",
+                            "Florida", "Georgia", "Hawaii", "Idaho", "Illinois",
+                            "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana",
+                            "Maine", "Maryland", "Massachusetts", "Michigan",
+                            "Minnesota", "Mississippi", "Missouri", "Montana",
+                            "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+                            "New Mexico", "New York", "North Carolina", "North Dakota",
+                            "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
+                            "South Carolina", "South Dakota", "Tennessee", "Texas",
+                            "Utah", "Vermont", "Virginia", "Washington", "West Virginia",
+                            "Wisconsin", "Wyoming")),
+    
+    sliderInput(inputId = "votePercent",
+                label = "Percentage of Voters: ",
+                min = 0,
+                max = 100,
+                value = 100),
+    
     mainPanel(
         textOutput("odds")
     )
@@ -75,16 +63,11 @@ ui <- fluidPage(
 
 server <- function(input, output) {
     
-    probability = dbinom(round(1000 * 0.5), size = 1000, prob = 0.5)
-    
     output$odds = renderText({
-        case_when(
-            input$state == "CA" ~ probability,
-            input$state == "AK" ~ 0.05
-        )
-        
+        totalVoters = input$votePercent * unname(stateVect[input$state])
+        votingOdds(totalVoters)
         })
-    
+
 }
 
 # Run the application 
