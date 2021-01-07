6aa336ebed994e932717bfa8888038e2baed6bb9
Econundrums <econundrums7@gmail.com>
2021-01-07 13:27:16 -0800

    Added choropleth map and lottery comparison table. App complete.

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -8,38 +8,36 @@
 #
 
 # To Do:
-# 2. Lottery comparisons.
 # 3. Make it look "pretty".
+# 4. Get choropleth map highlighting different state voter turnouts.
+# 5. Swap between light and dark theme?
 
 library(shiny)
+library(shinythemes)
 library(ggplot2)
-library(maps)
+library(plotly)
+library(usmap)
 library(dplyr)
 library(readxl)
 
-# Load population, US geo-coordinate, and Powerball data. 
+# Load voter turnout and Powerball data. 
 
-statePop = read_excel("../data/master.xlsx", sheet = "State Pop 18+")
+voterData = as.data.frame(read_excel("../data/master.xlsx", sheet = "Voter Turnout"))
 powerball = read_excel("../data/master.xlsx", sheet = "Powerball")
-stateGeo = map_data("state")
 
-# Merge population and geo-coordinate data.
+# Percentage adjustments to voter data.
 
-statePop$region = tolower(statePop$State)
-mergedData = inner_join(statePop, stateGeo, by = 'region')
-
-# Creating a named vector to easily call population data in the server.
-
-stateVect = statePop$Population
-names(stateVect) = statePop$State
+voterData$democratic_bias = round(voterData$democratic_bias * 100)
+voterData$vep_total_ballots_counted = round(voterData$vep_total_ballots_counted * 100)
 
 ui <- fluidPage(
-    titlePanel("Voting Sucks"),
+    titlePanel("Don't Vote. Play the Lottery Instead."),
+    theme = shinytheme("cosmo"),
     
     sidebarPanel(
         selectInput(inputId = "state", 
                     label = "State: ",
-                    choices = names(stateVect)),
+                    choices = voterData$state),
         
         sliderInput(inputId = "votePercent",
                     label = "Voter Turnout (%): ",
@@ -48,17 +46,24 @@ ui <- fluidPage(
                     value = 100),
         
         sliderInput(inputId = "demPercent",
-                    label = "Democrat Candidate Bias (%): ",
+                    label = "Bias Towards Democrats (%): ",
                     min = 0,
                     max = 100,
                     value = 50),
         
+        radioButtons(inputId = 'mapOptions',
+                     label = "Choose Map Data",
+                     choices = c("Voter Turnout", "Bias Towards Democrats")
+        ),
+        
         helpText("Odds Your Vote Matters"),
         textOutput("odds"),
+        
         ),
 
     mainPanel(
-        tableOutput("powerOdds")
+        tableOutput("powerOdds"),
+        plotlyOutput("map")
     )
 )
 
@@ -66,20 +71,43 @@ ui <- fluidPage(
 server <- function(input, output) {
     
     output$odds = renderText({
-        population = round((input$votePercent/100) * unname(stateVect[input$state]))
-        probability = dbinom(round(population*0.5), size = population, prob = 0.5)
-        paste(round(probability*100, 4), "%", " or 1 in ", round(1/probability), 
-              sep = "")
+        
+        voterTurnout = round((input$votePercent/100) * voterData[voterData$state == input$state, "vep"])
+        probability = dbinom(round(voterTurnout*0.5), size = voterTurnout, prob = 0.5)
+        paste("1 in ", round(1/probability), ', or ', round(probability*100, 4), "%", sep = "")
+        
         })
     
     output$powerOdds = renderTable({
-        population = round((input$votePercent/100) * unname(stateVect[input$state]))
+        voterTurnout = round((input$votePercent/100) * voterData[voterData$state == input$state, "vep"])
         demProb = input$demPercent/100
-        logProb = dbinom(round(population*0.5), size = population, prob = demProb, log = TRUE)
+        logProb = dbinom(round(voterTurnout*0.5), size = voterTurnout, prob = demProb, log = TRUE)
         powerball[,'More Likely to Win'] = paste(floor(logProb/log(powerball$Odds)), ' times', sep = '')
         powerball[,c('Match', 'Odds of Winning', 'Prize', 'More Likely to Win')]
+        },
+        
+        bordered = TRUE,
+        align = 'c',
+        striped = TRUE,
+        spacing = 'm',
+        width = '100%')
+    
+    output$map = renderPlotly({
+        if (input$mapOptions == 'Voter Turnout'){
+            p = plot_usmap(data = voterData, values = "vep_total_ballots_counted") + 
+                scale_fill_continuous(name = "Voter Turnout (%)", label = scales::comma) +
+                theme(legend.position = "right")
+        }
+        else{
+            p = plot_usmap(data = voterData, values = "democratic_bias") + 
+                scale_fill_continuous(low = "red", high = "blue", name = "Bias Towards Democrats (%)", label = scales::comma, breaks = c(0, 25, 50, 75, 100), limits = c(0, 100)) +
+                theme(legend.position = "right")
+        }
+        
+        ggplotly(p)
+        
     })
-
+    
 }
 
 # Run the application 
