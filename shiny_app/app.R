87865d03ca5c061ec71826c4ba40dbbc6a589027
Econundrums <econundrums7@gmail.com>
2021-01-09 15:43:56 -0800

    Modified app around general election data.

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -8,13 +8,12 @@ library(readxl)
 
 # Load voter turnout and Powerball data. 
 
-voterData = as.data.frame(read_excel("../data/master.xlsx", sheet = "Voter Turnout"))
+voterData = as.data.frame(read_excel("../data/master.xlsx", sheet = "Turnout Rates"))
 powerball = read_excel("../data/master.xlsx", sheet = "Powerball")
 
 # Percentage adjustments to voter data.
 
-voterData$democratic_bias = round(voterData$democratic_bias * 100)
-voterData$vep_total_ballots_counted = round(voterData$vep_total_ballots_counted * 100)
+voterData[, 'VEP Turnout Rate (Highest Office)'] = round(voterData[, 'VEP Turnout Rate (Highest Office)'] * 100)
 
 ui <- fluidPage(
     titlePanel("Don't Vote. Play the Lottery Instead."),
@@ -37,11 +36,6 @@ ui <- fluidPage(
                     max = 100,
                     value = 50),
         
-        radioButtons(inputId = 'mapOptions',
-                     label = "Choose Map Data",
-                     choices = c("Voter Turnout", "Bias Towards Democrats")
-        ),
-        
         helpText("Odds Your Vote Matters"),
         textOutput("odds"),
         
@@ -58,7 +52,7 @@ server <- function(input, output) {
     
     output$odds = renderText({
         
-        voterTurnout = round((input$votePercent/100) * voterData[voterData$state == input$state, "vep"])
+        voterTurnout = round((input$votePercent/100) * voterData[voterData$state == input$state, "Voting-Eligible Population (VEP)"])
         probability = dbinom(round(voterTurnout*0.5), size = voterTurnout, prob = 0.5)
         paste("1 in ", round(1/probability), ', or ', round(probability*100, 4), "%", sep = "")
         
@@ -66,7 +60,7 @@ server <- function(input, output) {
     
     output$powerOdds = renderTable({
         
-        voterTurnout = round((input$votePercent/100) * voterData[voterData$state == input$state, "vep"])
+        voterTurnout = round((input$votePercent/100) * voterData[voterData$state == input$state, "Voting-Eligible Population (VEP)"])
         demProb = input$demPercent/100
         logProb = dbinom(round(voterTurnout*0.5), size = voterTurnout, prob = demProb, log = TRUE)
         powerball[,'More Likely to Win'] = paste(floor(logProb/log(powerball$Odds)), ' times', sep = '')
@@ -81,16 +75,10 @@ server <- function(input, output) {
         )
     
     output$map = renderPlotly({
-        if (input$mapOptions == 'Voter Turnout'){
-            p = plot_usmap(data = voterData, values = "vep_total_ballots_counted") + 
-                scale_fill_continuous(name = "Voter Turnout (%)", label = scales::comma) +
-                theme(legend.position = "right")
-        }
-        else{
-            p = plot_usmap(data = voterData, values = "democratic_bias") + 
-                scale_fill_continuous(low = "red", high = "blue", name = "Bias Towards Democrats (%)", label = scales::comma, breaks = c(0, 25, 50, 75, 100), limits = c(0, 100)) +
-                theme(legend.position = "right")
-        }
+       
+        p = plot_usmap(data = voterData, values = 'VEP Turnout Rate (Highest Office)') + 
+            scale_fill_continuous(name = "Voter Turnout (%)", label = scales::comma) +
+            theme(legend.position = "right")
         
         ggplotly(p)
         
